907f90183f3d69fb918f6a0a8ad0c361d50096be
shamcloud <shamlantartisio@yahoo.com>
2019-08-29 06:09:09 -0500

    Stored value in a variable with the assignment operator.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,13 @@
+
+// I assign the value 7 to variable a, and assign the contents of a to variable b.
+
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a;
+var b = a;
+var b = 7;
+var a = 7;
