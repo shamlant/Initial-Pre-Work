b114b8fc3a95888b969a562cc6b728bab05893d1
shamcloud <shamlantartisio@yahoo.com>
2019-08-29 06:33:54 -0500

    Used camelCase to modify the existing declarations.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,13 @@
+
+/* In JavaScript all variables and function names are case sensitive.
+This means that capitalization matters.*/
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
