4a311a3ea6e7ad7cccb6ed301f6d3aed6cbd1bb8
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 00:43:46 -0500

    Build a string by using concatenation operator +.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,11 @@
+
+// I can insert one or more varialbes into a string I'm building.
+
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "My name is";
+var myStr = "Hi, my name is " + myName + ", and I am well!";
