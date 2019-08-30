83281350060f68b1b0dacf05b8bcb313c4e208c9
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 00:26:13 -0500

    Concatenating strings with the plus operator.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,10 @@
+
+// I can new build new string out of other strings by concatenating them together.
+
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = 'This is the start. ' + 'This is the end.';
