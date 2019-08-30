9ac104f7b94deee6d935737d1614496b20f3eb3a
shamcloud <shamlantartisio@yahoo.com>
2019-08-29 22:20:52 -0500

    Created two strings and assigned them values of first and last name.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,12 @@
+
+/* Created two strings variables: myFirstName and myLastName.
+and assign them values of first and last name.*/
+
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Make";
+var myLastName = "Lovin";
