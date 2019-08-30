3de3ea605ce410fa1eafb254e2a744bfbcaef41c
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 01:09:43 -0500

    Assign a new string to change myStr.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,10 @@
+
+/* In JavaScript, String values are immutable, which means that they cannot be altered once created.
+The only way to change myStr would be to assign it with a new string.*/
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
