b80d4fe998d47f3664afb3e0e6caf0f511e1e2c7
shamcloud <shamlantartisio@yahoo.com>
2019-08-29 23:15:45 -0500

    Created a escape sequences in the strings.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,6 @@
+
+/* escape allow you to use characters you might not otherwise be able to type out, such as a backspace.
+ Second is to allow you to represent multiple quotes in a string without JavaScript misinterpreting what you mean.*/
+
+
+var myStr = "FirstLine\n\t\SecondLine\n\ThirdLine"; // Change this line
