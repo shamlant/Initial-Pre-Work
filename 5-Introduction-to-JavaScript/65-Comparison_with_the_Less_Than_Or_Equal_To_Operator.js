6d3c2fdfe554138c3566cef6ef6fb908e33d7a57
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 10:26:17 -0500

    Used less than or equal to operator to converts data type.

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,17 @@
+
+// If the number on the left is greater than the number on the right, it returns false. Otherwise its true.
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
