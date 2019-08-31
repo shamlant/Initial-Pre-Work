e59c590818b1add39f8f12adc8e471fdec1b5ac7
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 10:38:18 -0500

    Combine the two if statments into one statment.

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,19 @@
+
+// logical and operator (&&) returns true if and only if the operands to the left and right of it are true.
+
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+
+      return "Yes";
+    }
+
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
