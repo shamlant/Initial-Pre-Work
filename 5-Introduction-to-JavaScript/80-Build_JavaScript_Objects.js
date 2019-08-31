7d3ea0b128bc0a79b60e79651c514660a3a31deb
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 14:59:32 -0500

    Build JavaScript Objects.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,23 @@
+
+// Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Blazz",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["humans"]
+
+
+
+};
