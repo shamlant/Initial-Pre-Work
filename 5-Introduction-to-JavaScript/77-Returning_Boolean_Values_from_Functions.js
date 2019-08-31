d18b69615b782e7e8794b1fd28fc8020431b39d7
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 13:14:40 -0500

    Return the Boolean values from the functions.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,15 @@
+
+/* But there's a better way to do this. Since === returns true or false,
+we can return the result of the comparison:
+function isEqual(a,b) {
+  return a === b;
+}.*/
+
+function isLess(a, b) {
+  // Fix this code
+  return (a < b);
+
+}
+
+// Change these values to test
+isLess(10, 15);
