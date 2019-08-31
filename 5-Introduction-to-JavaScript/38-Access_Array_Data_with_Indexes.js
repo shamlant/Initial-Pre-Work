b51d5ecdb99f20ae3bdc1d8b41ac7b34b56af18e
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 20:51:43 -0500

    Used the index to access data inside arrays.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,13 @@
+
+// I can access the data inside arrays using indexes.
+
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
