4894445af7a086d4d34181acbea2e4de1105de4d
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 10:01:57 -0500

    Convert data tryp of values while comparing using the inequality operator.

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,14 @@
+
+/* The inequality operator (!=) is the opposite of the equality operator.
+It means "Not Equal" and returns false where equality would return true and vice versa.*/
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
