051f96baddcd43cd663a572d74d36e965c645add
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 15:53:53 -0500

    Update the object using the dot and bracket notation.

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,25 @@
+
+/* After I create a JavaScript object, I can update its properties at any time just like I would update
+any other variable. I can also use either dot or bracket notation to update.*/
+
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";
