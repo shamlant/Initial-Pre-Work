1b69c23953be23d3e6b4c71464d2cd32c34b6958
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 23:56:52 -0500

    Created a function timesFive that accepts one argument.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,16 @@
+
+// Created a function timesFive that accepts one argument, multiplies it by 5, and returns the new value
+
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
