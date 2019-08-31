c75ab11ad90a4b8aa01d6815b33d8139bc1c313f
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 00:29:29 -0500

    Adds a number to the end of the array and then removed the first element of the array.

---
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -0,0 +1,19 @@
+
+/* In Computer Science a queue is an abstract Data Structure where items are kept in order.
+New items can be added at the back of the queue and old items are taken off from the front of the queue.*/
+
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  var removed = arr.shift();
+  return removed;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
