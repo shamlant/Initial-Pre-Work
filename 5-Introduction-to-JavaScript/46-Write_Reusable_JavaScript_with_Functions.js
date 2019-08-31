7c0561455342dd502bd6abcc363852c0274600da
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 21:54:47 -0500

    Used reusable JavaScript to divide the codes in the function.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,18 @@
+
+/* In JavaScript, I can divide up our code into reusable parts called functions.
+You can call or invoke this function by using its name followed by parentheses, like this:functionName();.*/
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+
+reusableFunction();
