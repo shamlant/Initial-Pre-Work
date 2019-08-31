423cc6f711e3b4abc3b88b5196e5c922c32e926c
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 21:42:34 -0500

    Created mylist using the Multi-dimensional arrau containing several sub-arrays.

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,5 @@
+
+// Created a shopping lists using mylist with multi-dimensional array containing several sub-arrays.
+
+
+var myList = [["Cereal", 2],["Fruits", 5],["Rice", 4],["Water", 12],["Juice", 15]];
