8d195fa97494a3ea88d5bb58e725af65049be9fb
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 20:35:47 -0500

    Used the concatenation operator plus to build new string.

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,13 @@
+
+// Created mad libs style using the concatenation operator + to build new string.
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
