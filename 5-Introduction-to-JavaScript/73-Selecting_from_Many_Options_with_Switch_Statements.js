fffde8e2eada791e69e9917429bd570887e56b93
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 12:14:53 -0500

    Used Switch statement to select from many options of the values.

---
+++ 5-Introduction-to-JavaScript/73-Selecting_from_Many_Options_with_Switch_Statements.js
@@ -0,0 +1,29 @@
+
+/* If i have many options to choose from i can use the switch statement to tests the value
+can have many case statements which define various possible values.*/
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    return "alpha";
+    break;
+    case 2:
+    return "beta";
+    break;
+    case 3:
+    return "gamma";
+    break;
+    case 4:
+    return "delta";
+    break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
