4dda1ae7dc474756d4da62e835f44bdd60e9e5ee
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 00:52:54 -0500

    Used the if statements to tell JavaScript to excute the code curly braces under certain conditions.

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,28 @@
+
+/* If statements are used to make decisions in code.
+The keyword if tells JavaScript to execute the code in the curly braces under certain conditions,
+defined in the parentheses.*/
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  if (wasThatTrue) {
+      return "Yes, that was true";
+  }
+  return "No, that was false";
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
