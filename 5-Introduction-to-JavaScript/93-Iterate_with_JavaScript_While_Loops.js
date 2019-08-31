f8639da1f55f075e1f23c624d1e97fe60813eb56
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 18:03:33 -0500

    Used While Loop to run the same code multiple times.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,15 @@
+
+/*first type of loop we will learn is called a "while" loop
+because it runs "while" a specified condition is true and stops once that condition is no longer true.*/
+
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var i = 0;
+while(i <= 4) {
+  myArray.push(i);
+  i++;
+}
