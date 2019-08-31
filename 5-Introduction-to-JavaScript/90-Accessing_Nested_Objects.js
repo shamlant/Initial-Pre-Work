22dcfe987bf3c4145e10815682e841f092bd3933
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 17:20:45 -0500

    Acessed Nested Objects by chaining them together.

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,18 @@
+
+// the sub-properties of objects can be accessed by chaining together the dot or bracket notation.
+
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
