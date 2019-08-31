17d0b543ba43c1d005137ec7231c26184e429fba
shamcloud <shamlantartisio@yahoo.com>
2019-08-30 22:53:25 -0500

    Declared a local variable myVar inside myLocalScope.

---
+++ 5-Introduction-to-JavaScript/49-Local_Scope_and_Functions.js
@@ -0,0 +1,17 @@
+
+/* Variables which are declared within a function, as well as the function parameters have local scope.
+That means, they are only visible within that function.*/
+
+
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar;
+
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+// Now remove the console log line to pass the test
