65fa67b9c03ae1904d48252f3c7ac992d2792d55
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 11:21:41 -0500

    Introducing Else Statements.

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,20 @@
+
+// Did everything right except didnt get the testElse(5) or smaller right return result.
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+ else {
+  result = "5 or smaller";
+}
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
