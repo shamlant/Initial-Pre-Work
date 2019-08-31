ed9397a56502f53197f9b3fb1daf52287ad3a4c5
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 12:01:47 -0500

    Used the Golf Code to combine the if/else statements together.

---
+++ 5-Introduction-to-JavaScript/72-Golf_Code.js
@@ -0,0 +1,29 @@
+
+// par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
+
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  }
+  else if (strokes <=par-2) {
+    return "Eagle";
+  } else if (strokes == par -1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(5, 4);
