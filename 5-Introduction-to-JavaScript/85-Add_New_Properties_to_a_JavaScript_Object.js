8ef7df82a60b3969a5f28778338f36b34505d80c
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 15:57:50 -0500

    Adds new properties to JavaScript Object.

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,25 @@
+
+// I can add new property to JavaScript objects the same way i would modify them.
+
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.bark = "woof";
