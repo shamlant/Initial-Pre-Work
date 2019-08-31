5437736dcbb82788d442003fc0d59b0841f247de
shamcloud <shamlantartisio@yahoo.com>
2019-08-31 16:35:37 -0500

    Manipulate Complex Objects.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,29 @@
+
+
+/* A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of
+strings, numbers, booleans, arrays, functions, and objects.*/
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Bob Marley",
+    "title": "No women a cry",
+    "release_year": 1980,
+    "formats": [
+      "CD",
+      "Cassette",
+      "Disc",
+    ],
+  }
+  // Add record here
+];
