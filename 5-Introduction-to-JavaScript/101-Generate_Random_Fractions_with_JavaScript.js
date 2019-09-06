582b0d70e682b898af16b71d7e3917cf5d8755bc
shamcloud <shamlantartisio@yahoo.com>
2019-09-06 00:18:36 -0500

    Generate Random fractions with JavaScript.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,15 @@
+
+// Random numbers are useful for creating random behavior.
+
+
+function randomFraction() {
+
+  // Only change code below this line.
+var result = 0;
+while (result === 0) {
+    result = Math.random();
+}
+  return result;
+
+  // Only change code above this line.
+}
