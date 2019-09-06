cad5036c026793dee2abd96a3e2b12b3f5d72a31
shamcloud <shamlantartisio@yahoo.com>
2019-09-05 23:09:51 -0500

    Used JavaScript to change Whileloop to a do....whileloop in myArray.

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,16 @@
+
+
+/* I can Change the whileloop in the code to a do...whileloop so that the loop will push
+the number 10 to myArray, and iwill be equal to 11when your code finishes running.*/
+
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 11);
