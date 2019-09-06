011eae1e06938e20d43c4307f4124588f35a03fb
shamcloud <shamlantartisio@yahoo.com>
2019-09-06 00:38:44 -0500

    Generate random Whole Number within a Range.

---
+++ 5-Introduction-to-JavaScript/103-Generate_Random_Whole_Numbers_within_a_Range.js
@@ -0,0 +1,22 @@
+
+// I can generate a random number that falls within a range of two specific numbers.
+
+
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin +1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
