3dd3e2bc6e21a6aabf95d1c8587e740cfe924488
shamcloud <shamlantartisio@yahoo.com>
2019-09-05 23:29:58 -0500

    Used For Loop to lookup contacts list and prop.

---
+++ 5-Introduction-to-JavaScript/100-Profile_Lookup.js
@@ -0,0 +1,52 @@
+
+/* Used a for loop to cycle through the contacts list, and nested if statement
+to first check if the firstName matches, and then checks if the prop matches.*/
+
+
+
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+for (var x = 0; x < contacts.length; x++){
+  if (contacts[x].firstName === name){
+    if (contacts[x].hasOwnProperty(prop)) {
+      return contacts[x][prop];
+    } else {
+      return "No such property";
+    }
+  }
+}
+return "No such contact";
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");
