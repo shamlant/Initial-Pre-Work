599d0520e96d1a004926d87e44465ef6cedc2553
shamcloud <shamlantartisio@yahoo.com>
2019-09-10 19:59:09 -0500

    Change the var to let in the function.

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,13 @@
+
+// change the va to let in the function to locate an error in larger codes.
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
