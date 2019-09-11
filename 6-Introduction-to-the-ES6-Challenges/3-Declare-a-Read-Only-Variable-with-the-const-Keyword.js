dec25f24a30fbc2c287223c24bb691aa23d37731
shamcloud <shamlantartisio@yahoo.com>
2019-09-10 20:20:09 -0500

    Assigned a const to the function.

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,18 @@
+
+// Constant value means that once a function assigned const, it can't be reassigned.
+
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
