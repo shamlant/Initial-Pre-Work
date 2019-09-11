34c8a173d4ee89d1296f3a81a0fec7c48ffc8183
shamcloud <shamlantartisio@yahoo.com>
2019-09-10 23:47:23 -0500

    Set a Defalut Parameters to the function.

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,11 @@
+
+// The default parameter kicks in when the argument is not specified (it is undefined).
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value=1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
