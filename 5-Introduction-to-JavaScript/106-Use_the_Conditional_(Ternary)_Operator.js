55450641e96ae64de76021ca77904da7a61178d6
shamcloud <shamlantartisio@yahoo.com>
2019-09-10 19:37:53 -0500

    Used the Conditional Operator to checkEqual the function.

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,11 @@
+
+// The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+
+
+function checkEqual(a, b) {
+  return (a === b ? true : false );
+}
+
+checkEqual(1, 2);
+checkEqual(1, -1);
+checkEqual(1, 1);
