df0324dc1816aaaf0db4e23cd4282d0ef0a5c5fe
shamcloud <shamlantartisio@yahoo.com>
2019-09-10 22:17:13 -0500

    Used Arrow Function for more than one function,

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,9 @@
+
+// I can pass more than one argument into arrow function as well.
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
