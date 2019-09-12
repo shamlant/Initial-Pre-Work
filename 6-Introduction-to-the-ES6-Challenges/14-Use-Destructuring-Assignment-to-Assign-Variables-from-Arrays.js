bc98ec038a6c764762556303665718a01ea1a1d5
shamcloud <shamlantartisio@yahoo.com>
2019-09-11 23:17:43 -0500

    Used Destructuring Assignment to assign the values.

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,13 @@
+
+// Used destructing assignment to swap the values of a and b.
+
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [b, a] = [a, b];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
