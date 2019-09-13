08d2d019669622228bd89ec5d4d1205efe9ce9aa
shamcloud <shamlantartisio@yahoo.com>
2019-09-12 19:38:51 -0500

    Adds the Template Literals to create strings.

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,27 @@
+
+
+/* Check answer. template literal. This is a special type of string that makes creating complex strings easier.*/
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = arr.map((text) => {
+    return '<li class="text-warning">${text}</li>'
+  });
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
