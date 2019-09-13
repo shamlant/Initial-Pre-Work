6ef8e45496eac92f17f990da3a4b4baf47b32735
shamcloud <shamlantartisio@yahoo.com>
2019-09-12 19:50:15 -0500

    Removed the function with the Declarative Fucntions in ES6.

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,15 @@
+
+// I can remove the functionkeyword and colon altogether when defining functions in objects.
+
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
