3db25dd62a778ae20cb7e31c9ab20c414dbe2a70
shamcloud <shamlantartisio@yahoo.com>
2019-09-12 19:45:36 -0500

    Used the Simple Fields with Object Literals to write concise.

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,11 @@
+
+// Use simple fields with object literals to create and return a Personobject.
+
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return { name, age ,gender };
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
