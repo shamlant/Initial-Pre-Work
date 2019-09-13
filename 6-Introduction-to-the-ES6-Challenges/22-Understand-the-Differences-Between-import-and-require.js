e6ef8aeb21db572e6441953c5ca1f1794378ca3a
shamcloud <shamlantartisio@yahoo.com>
2019-09-12 21:29:14 -0500

    Adds import statement.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+
+//  with import we can choose which parts of a module or file to load into a given file, saving time and memory.
+
+"use strict";
+import { capitalizeString } from "string_functions"
+capitalizeString("hello!");
