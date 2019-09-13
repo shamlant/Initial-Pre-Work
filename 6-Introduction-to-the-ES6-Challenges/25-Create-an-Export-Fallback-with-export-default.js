5332c24e08d2ec318e04bb681f8f62af276ea4eb
shamcloud <shamlantartisio@yahoo.com>
2019-09-12 22:21:09 -0500

    Used an Export Default to create an Export Fallback.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,7 @@
+
+/* There is another exportsyntax you need to know, known as export default.
+Usually you will use this syntax if only one value is being exported from a file.
+It is also used to create a fallback value for a file or module.*/
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
