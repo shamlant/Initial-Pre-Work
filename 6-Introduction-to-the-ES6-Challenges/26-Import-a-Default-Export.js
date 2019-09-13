090782d56d4e486c1e463ec50e0d25c67270d3c4
shamcloud <shamlantartisio@yahoo.com>
2019-09-12 22:37:47 -0500

    Import a Default Export.

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,8 @@
+
+/* import the default export, subtract, from the file "math_functions",
+found in the same directory as this file.*/
+
+
+"use strict";
+ import subtract from "math_functions"
+ subtract(7,4);
